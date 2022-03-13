24db957ef4622ba544b45249b8d429472a92ace4
Mahmoud Matarawy <eng.mahmoud.adel94@gmail.com>
2022-03-13 06:33:53 +0200

    Initial commit

---
+++ Atmega32/Drivers/LCD/LCD4/LCD4/main.c
@@ -0,0 +1,27 @@
+/*
+ * LCD4.c
+ *
+ * Created: 3/13/2022 2:54:54 AM
+ * Author : Mahmoud Sarhan
+ * Email  : eng.mahmoud.adel94@gmail.com 
+ */ 
+
+
+#include "lcd4.h"
+
+int main(void)
+{
+	DDRA = 0xff;
+	Lcd4_Init();
+    while (1) 
+    {
+		Lcd4_Set_Cursor(1,1);
+		Lcd4_Write_String("Mahmoud");
+		_delay_ms(200);
+		Lcd4_Set_Cursor(2,1);
+		Lcd4_Write_String("Sarhan");
+		 _delay_ms(200);
+		 Lcd4_Clear();
+    }
+}
+
